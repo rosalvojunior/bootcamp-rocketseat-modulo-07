df3bd5c2c1a8eb3dc860f8c9ff2486188e1c8617
Rosalvo Júnior <rosalvo@juniorbh.com.br>
2019-10-06 09:47:44 -0300

    Configurando Redux Saga

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -2,7 +2,7 @@ import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
-    case '@cart/ADD':
+    case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.product.id);
 
@@ -23,7 +23,7 @@ export default function cart(state = [], action) {
         }
       });
     case '@cart/UPDATE_AMOUNT': {
-      if(action.amount <= 0){
+      if (action.amount <= 0) {
         return state;
       }
       return produce(state, draft => {
