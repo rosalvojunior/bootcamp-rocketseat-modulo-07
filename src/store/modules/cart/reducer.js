43c67a1ad06a88e84441f2c2485267847a069fc2
Rosalvo Júnior <rosalvo@juniorbh.com.br>
2019-10-06 10:10:34 -0300

    Separando actions

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,16 +4,18 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        const { product } = action;
+        draft.push(product);
+        // const productIndex = draft.findIndex(p => p.id === action.product.id);
 
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        // if (productIndex >= 0) {
+        //   draft[productIndex].amount += 1;
+        // } else {
+        //   draft.push({
+        //     ...action.product,
+        //     amount: 1,
+        //   });
+        // }
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
